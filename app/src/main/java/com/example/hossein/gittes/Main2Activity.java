4cf1f9744069331fac3061ad6b422b47dbe63203
Hossein <hossein.hit7531@gmail.com>
2017-09-28 10:56:02 +0330

    log ezafe shod

--- app/src/main/java/com/example/hossein/gittes/Main2Activity.java
+++ app/src/main/java/com/example/hossein/gittes/Main2Activity.java
@@ -2,15 +2,13 @@ package com.example.hossein.gittes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-  private static final String TAG = "MainActivity";
+public class Main2Activity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-////
+    setContentView(R.layout.activity_main2);
+    //
   }
 }
